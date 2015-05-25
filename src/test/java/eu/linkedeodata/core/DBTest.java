77df976679211f647560fc3358a3586396fd9976
Giannis Vlachopoulos <giannis@sancho.di.uoa.gr>
2015-05-25 17:00:45 +0300

    added forgotten files.

---
+++ src/test/java/eu/linkedeodata/core/DBTest.java
@@ -0,0 +1,48 @@
+package eu.linkedeodata.core;
+
+import java.sql.SQLException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import d2rq.dump_rdf;
+import static org.junit.Assert.assertTrue;
+
+public abstract class DBTest extends TemplateTest {
+
+	protected String dbName;
+	protected boolean postgres;					//postgis or monetdb (true for postgis)
+	protected String username;
+	protected String password;
+	
+	public DBTest(String dbName, String username, String password, boolean postgres, String baseIRI, String mappingFile) {
+		this.dbName = dbName;
+		this.postgres = postgres;
+		this.username = username;
+		this.password = password;
+		this.baseIRI = baseIRI;
+		this.mappingFile = mappingFile;
+	}
+	
+	@Before @Override
+	public void before() throws SQLException {
+		Utils.loaddb(dbName, postgres);
+	}
+	
+	@Test @Override
+	public void test() throws Exception {
+		String [] args = { "dump_rdf", "-b", this.baseIRI, "-o", this.outputFile, "-u", this.username, "-p", this.password, "-jdbc", this.postgres ? "jdbc:postgresql://"+Utils.serverName + ":"+Utils.postgresPort+ "/" + this.dbName : "jdbc:monetdb://"+Utils.serverName + ":" + Utils.monetdbPort + "/" + this.dbName, this.mappingFile };
+		new dump_rdf().process(args);
+		Utils.sortFile(originalRdfGraphFile);
+		Utils.sortFile(this.outputFile);
+		boolean expr = Utils.checkDiff(originalRdfGraphFile+"_sorted.nt", this.outputFile+"_sorted.nt");
+		assertTrue("SHP Test:" + this.getClass() + ": RDF graph file and results do not match", !expr);
+	}
+	
+	@After @Override
+	public void after() throws SQLException {
+		Utils.unloaddb();
+		super.after();
+	}
+}
---
+++ src/test/java/eu/linkedeodata/core/Utils.java
@@ -0,0 +1,115 @@
+package eu.linkedeodata.core;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertFalse;
+
+public class Utils {
+
+	public static String serverName = "localhost";
+	public static Connection conn = null;
+	
+	/*
+	 * postgis specific
+	 */
+	public static String dbTemplate = "template_postgis";
+	public static String postgresUsername = "postgres";
+	public static String postgresPassword = "postgres";
+	public static int postgresPort = 5432;
+	
+	/*
+	 * monetdb specific
+	 */
+//	private static String monetdbUsername = "monetdb";
+//	private static String monetdbPassword = "monetdb";
+	public static int monetdbPort = 50000;	
+	
+	public static void createdb(String name, String sqlPath, boolean postgres) throws SQLException {
+		String url = "";
+		List<String> databases = new ArrayList<String>();
+		if (postgres) {
+			url = "jdbc:postgresql://"+serverName +":"+postgresPort;
+			conn = DriverManager.getConnection(url, postgresUsername, postgresPassword);
+			PreparedStatement pst = conn.prepareStatement("SELECT * FROM pg_catalog.pg_database");
+	        ResultSet rs = pst.executeQuery();
+
+	        while (rs.next())
+	        {
+	        	databases.add(rs.getString(1));
+	        }
+	        
+	        assertFalse("Database " + name + " already exists. Use another name or drop the database first", databases.contains(name));
+	        pst = conn.prepareStatement("CREATE DATABASE "+name+" TEMPLATE " + dbTemplate);
+			pst.executeUpdate();
+			pst.close();
+			conn.close();
+
+			url = "jdbc:postgresql://"+serverName+":"+postgresPort+"/"+name;
+			conn = DriverManager.getConnection(url, postgresUsername, postgresPassword);
+		}
+		else {
+			//create monetdb db;
+		}
+	}
+	
+	public static void dropdb(String name, boolean postgres) throws SQLException {
+		conn.close();
+		if (postgres) {
+			String url = "jdbc:postgresql://"+serverName+":"+postgresPort;
+			conn = DriverManager.getConnection(url, postgresUsername, postgresPassword);
+			
+			PreparedStatement pst = conn.prepareStatement("DROP DATABASE "+name);
+			pst.executeUpdate();
+			pst.close();
+			conn.close();
+		}
+		else {
+			//monetdb
+		}
+		
+	}
+	
+	public static void loaddb(String name, boolean postgres) throws SQLException {
+		String url = "";
+		if (postgres) {
+			url = "jdbc:postgresql://"+serverName+":"+postgresPort+"/"+name;
+			conn = DriverManager.getConnection(url, postgresUsername, postgresPassword);
+		}
+	}
+
+	public static void unloaddb() throws SQLException {
+		conn.close();		
+	}
+	
+	public static void sortFile(String filename) throws IOException, InterruptedException {
+		Process p = Runtime.getRuntime().exec("sort " + filename + " -o " + filename + "_sorted.nt");
+		p.waitFor();
+	}
+	
+	public static boolean checkDiff(String file1, String file2) throws IOException, InterruptedException {
+		Process p = Runtime.getRuntime().exec("diff " + file1 + " " + file2);
+		//p.waitFor();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+		
+		StringBuilder sb = new StringBuilder("");
+		String line = "";
+		while ((line = reader.readLine())!= null) {
+			sb.append(line + "\n");
+		}
+		if (sb.toString().equals("")) {
+			return false;
+		}
+		return true;
+		
+	}
+	
+}
