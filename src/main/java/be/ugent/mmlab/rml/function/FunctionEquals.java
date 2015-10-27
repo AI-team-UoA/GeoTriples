dfbab7cfe28f1abf95fd6a3e2c4b912a0e27b0c5
Dimitrianos Savva <dimis@di.uoa.gr>
2015-10-27 13:01:03 +0200

    added support for most of the GeoSPARQL functions

---
+++ src/main/java/be/ugent/mmlab/rml/function/FunctionEquals.java
@@ -0,0 +1,37 @@
+package be.ugent.mmlab.rml.function;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.geotools.referencing.CRS;
+import org.opengis.referencing.FactoryException;
+import org.opengis.referencing.NoSuchAuthorityCodeException;
+import org.openrdf.model.Value;
+import org.openrdf.model.impl.LiteralImpl;
+import org.xml.sax.SAXException;
+
+import be.ugent.mmlab.rml.core.MalformedGeometryException;
+import be.ugent.mmlab.rml.vocabulary.Vocab.QLTerm;
+
+import com.vividsolutions.jts.geom.Geometry;
+
+
+public class FunctionEquals extends GeometryFunction implements Function {
+	
+	@Override
+	public List<? extends Object> execute(
+			List<? extends Object> arguments,List<? extends QLTerm> qlterms) throws SAXException, IOException, ParserConfigurationException, FactoryException, MalformedGeometryException {
+		List<String> valueList = new ArrayList<>();
+		
+		Geometry geometry1 = computeGeometry(arguments.get(0), qlterms.get(0));
+		Geometry geometry2 = computeGeometry(arguments.get(1), qlterms.get(1));
+		valueList.add(GTransormationFunctions.equals(geometry1, geometry2));
+		return valueList;
+	}
+
+
+}
