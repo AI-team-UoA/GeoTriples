9be753942be83c0d1cbd9b286619332a23c949f0
Dimitrianos Savva <dimis@di.uoa.gr>
2015-11-17 22:22:11 +0200

    print time stats class

---
+++ src/main/java/be/ugent/mmlab/rml/tools/PrintTimeStats.java
@@ -0,0 +1,16 @@
+package be.ugent.mmlab.rml.tools;
+
+public class PrintTimeStats {
+	private static double total_duration=0.0;
+
+	public static void printTime(String text, double time_milliseconds) {
+		System.out.println("Time: " + text + " took " + time_milliseconds + " msec");
+	}
+
+	public static void addDuration(double duration) {
+		total_duration+=duration;
+	}
+	public static double getDuration(){
+		return total_duration;
+	}
+}
